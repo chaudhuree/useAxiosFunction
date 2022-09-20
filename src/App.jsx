16bf371ffaed162ec96b1aeffe56c4266f4ca16d
chaudhuree <chaudhuree00@gmail.com>
2022-09-21 03:03:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import JsonPlaceHolder from "./components/JsonPlaceHolder"
+function App() {
+  return (
+    <div className="App">
+      <p>axios hook function</p>
+      <JsonPlaceHolder/>
+    </div>
+  );
+}
+
+export default App;
